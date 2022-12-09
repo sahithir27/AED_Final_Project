bfc3c03420be9040c2de346a18c929a95563ca51
Sahithi Gaddam <gaddam.sahi@northeastern.edu>
2022-12-09 12:03:36 -0500

    added organization

--- BloodDonationSystem/src/model/Organization/Organization.java
+++ BloodDonationSystem/src/model/Organization/Organization.java
@@ -4,10 +4,122 @@
  */
 package model.Organization;
 
+import java.util.ArrayList;
+import model.Employee.EmployeeDirectory;
+import model.Person.PatientsDirectory;
+import model.Role.Role;
+import model.Users.UsersDirectory;
+import model.WorkQueue.WorkQueue;
+
 /**
  *
  * @author sahithigaddam
  */
-public class Organization {
+public abstract class Organization {
+    private String name;
+    private WorkQueue workQueue;
+    private EmployeeDirectory listOfEmployees;
+    private UsersDirectory listOfUsers;
+    private PatientsDirectory listOfPatients;
+    private int orgID;
+    private static int count=0;
+    private OrganizationType organizationType;
+
     
+    public enum OrganizationType{
+        Medical("Medical"), 
+        Diagnostics("Diagnostics"), 
+        Facilities("Facilities"),
+        OrganManagement("Organ Management"),
+        PoliciesFinance("Policies and Finance"),
+        AwarnessCampaign("Awarness Campaign"),
+        User("User"),
+        Therapist("Therapist");
+        private String type;
+        private OrganizationType(String type) {
+            this.type = type;
+        }
+        public String getValue() {
+            return type;
+        }
+    }
+
+    public Organization(String name) {
+        this.name = name;
+        workQueue = new WorkQueue();
+        listOfEmployees = new EmployeeDirectory();
+        listOfUsers = new UsersDirectory();
+        listOfPatients=new PatientsDirectory();
+        orgID = count;
+        ++count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    public EmployeeDirectory getListOfEmployees() {
+        return listOfEmployees;
+    }
+
+    public void setListOfEmployees(EmployeeDirectory listOfEmployees) {
+        this.listOfEmployees = listOfEmployees;
+    }
+
+    public UsersDirectory getListOfUsers() {
+        return listOfUsers;
+    }
+
+    public void setListOfUsers(UsersDirectory listOfUsers) {
+        this.listOfUsers = listOfUsers;
+    }
+
+    public PatientsDirectory getListOfPatients() {
+        return listOfPatients;
+    }
+
+    public void setListOfPatients(PatientsDirectory listOfPatients) {
+        this.listOfPatients = listOfPatients;
+    }
+
+    public int getOrgID() {
+        return orgID;
+    }
+
+    public void setOrgID(int orgID) {
+        this.orgID = orgID;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Organization.count = count;
+    }
+
+    public OrganizationType getOrganizationType() {
+        return organizationType;
+    }
+
+    public void setOrganizationType(OrganizationType organizationType) {
+        this.organizationType = organizationType;
+    }
+    
+    @Override
+    public String toString() {
+        return name;
+    }
 }
--- BloodDonationSystem/src/ui/MainJFrame.java
+++ BloodDonationSystem/src/ui/MainJFrame.java
@@ -5,6 +5,9 @@
 package ui;
 
 import java.awt.CardLayout;
+import javax.swing.JOptionPane;
+import model.Organization.Organization;
+import model.Users.Users;
 
 /**
  *
@@ -205,7 +208,7 @@ public class MainJFrame extends javax.swing.JFrame {
                     if(userAccount==null){
                         //Step 3:check against each organization for each enterprise
                         for(Organization organization:enterprise.getoDirectory().getListOfOrganizations()){
-                            userAccount=organization.getUserAccountDirectory().authenticateUser(userName, password);
+                            userAccount=organization.getListOfUsers().authenticateUser(userName, password);
                             if(userAccount!=null){
                                 inNetwork=network;
                                 inEnterprise=enterprise;
@@ -234,7 +237,7 @@ public class MainJFrame extends javax.swing.JFrame {
         }
         else{
             CardLayout layout=(CardLayout)loginContainer.getLayout();
-            loginContainer.add("workArea",userAccount.getRole().createWorkArea(loginContainer, userAccount, inOrganization, inEnterprise, system, inNetwork, donorDirectory));
+            loginContainer.add("workArea",userAccount.getUserRole().createWorkArea(loginContainer, userAccount, inOrganization, inEnterprise, system, inNetwork, donorDirectory));
             layout.next(loginContainer);
         }
 
